ff7fd92f7a85d4821f3044e08bf1c829b47cdfe6
Jabrayilzada Suleyman <95436003+JSuleyman@users.noreply.github.com>
2023-03-13 22:36:18 +0400

    New Project

---
+++ src/main/java/com/example/travelguidewebapplication/TravelGuideWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelguidewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelGuideWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelGuideWebApplication.class, args);
+    }
+
+}
