c9518941330999feb09e7b9c2b5f8214702ee062
Jabrayilzada Suleyman <95436003+JSuleyman@users.noreply.github.com>
2023-07-06 11:09:19 +0400

    update for deploy

--- src/main/java/com/example/travelguidewebapplication/TravelGuideWebApplication.java
+++ src/main/java/com/example/travelguidewebapplication/TravelGuideWebApplication.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class TravelGuideWebApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TravelGuideWebApplication.class, args);
-    }
-
     @RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelGuideWebApplication.class, args);
     }
 
 }
