c41733b0c2aeba196be8c90bb303cc22e2c801f9
Jabrayilzada Suleyman <95436003+JSuleyman@users.noreply.github.com>
2023-07-06 11:38:47 +0400

    update for deploy

--- src/main/java/com/example/travelguidewebapplication/TravelGuideWebApplication.java
+++ src/main/java/com/example/travelguidewebapplication/TravelGuideWebApplication.java
@@ -2,19 +2,9 @@ package com.example.travelguidewebapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 public class TravelGuideWebApplication {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(TravelGuideWebApplication.class, args);
