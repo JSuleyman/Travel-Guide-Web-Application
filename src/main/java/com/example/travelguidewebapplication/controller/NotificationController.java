afa6aee4178975a51f7226c322e10cdf0066a8b3
Jabrayilzada Suleyman <95436003+JSuleyman@users.noreply.github.com>
2023-07-13 15:43:38 +0400

    update notification

--- src/main/java/com/example/travelguidewebapplication/controller/NotificationController.java
+++ src/main/java/com/example/travelguidewebapplication/controller/NotificationController.java
@@ -4,8 +4,8 @@ import com.example.travelguidewebapplication.DTO.response.NotificationResponseDT
 import com.example.travelguidewebapplication.service.inter.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +16,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @GetMapping
     public ResponseEntity<List<NotificationResponseDTO>> testNotification() {
-        return ResponseEntity.ok(notificationService.newCommentNotification());
-    }
-
-    @PostMapping
-    public void updateNotificationStatus() {
-        notificationService.notificationFalse();
+        List<NotificationResponseDTO> notifications = notificationService.newCommentNotification();
+        messagingTemplate.convertAndSend("/topic/notifications", notifications);
+        return ResponseEntity.ok(notifications);
     }
 }
