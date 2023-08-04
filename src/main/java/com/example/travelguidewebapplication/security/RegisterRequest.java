24122222f976756508c97526dc9e86aca34ea4ec
Jabrayilzada Suleyman <95436003+JSuleyman@users.noreply.github.com>
2023-08-04 16:26:25 +0400

    add email sender and user email verification

--- src/main/java/com/example/travelguidewebapplication/security/RegisterRequest.java
+++ src/main/java/com/example/travelguidewebapplication/security/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.example.travelguidewebapplication.security;
 
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,8 @@ public class RegisterRequest {
 
     private String firstname;
     private String lastname;
+
+    @Email
     private String email;
     private String password;
 }
